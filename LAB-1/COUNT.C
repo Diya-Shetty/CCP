cd1e18a48b8b888860a545ac63306fad2d0af10b
Diya-Shetty <83899337+Diya-Shetty@users.noreply.github.com>
2021-05-10 10:33:54 +0530

    Add files via upload

---
+++ LAB-1/COUNT.C
@@ -0,0 +1,26 @@
+//Take 10 numbers from user, then count number of positves,negatives and zeros
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i=1,a,positive=0,negative=0,zero=0;
+	clrscr();
+	while(i<=10)
+	{
+		printf("\n Enter a number :");
+		scanf("%d", &a);
+		if(a>0)
+			positive++;
+		else if(a<0)
+			negative++;
+		else if(a==0)
+			zero++;
+		else
+			printf("not valid");
+		i++;
+	}
+	printf("%d positives\t",positive);
+	printf("%d negatives\t",negative);
+	printf("%d zeros",zero);
+	getch();
+}
